dbe668f189d74f0a7f3c6dca8d979e6b412a08f7
Jack Romo <sharrackor@gmail.com>
2016-06-06 13:04:07 +0100

    Add slightly rotated text

--- memegen/image.py
+++ memegen/image.py
@@ -8,14 +8,30 @@ def gen_meme(file_src, file_dir, strings):
     Generate a meme from an image and some text.
     """
     image = Image.open(file_src)
-    drawer = ImageDraw.Draw(image)
-    width, height = image.size
-    font_size = width // 30
-    font = ImageFont.truetype("resources/comic_sans_font.ttf", font_size)
     for string in strings:
-        max_x = width - (len(string)*font_size)
-        max_y = height - font_size
-        x, y = random.randint(0, max_x), random.randint(0, max_y)
-        drawer.text((x, y), string, (255, 255, 255), font=font)
+        image = draw_text_on(image, string)
     image.save(file_dir, "JPEG")
+
+def draw_text_on(image, text):
+    """
+    Args:
+        image (Image): Image to draw on.
+        text (str): String to draw.
+
+    Returns:
+        Image: Image with string drawn at a random location and rotation.
+    """
+    width, height = image.size
+    font_size = width // 15
+    font = ImageFont.truetype("resources/comic_sans_font.ttf", font_size)
+    text_base = Image.new('RGBA', image.size, (255, 255, 255, 0))   # Base transparent image to write text on
+    drawer = ImageDraw.Draw(text_base)
+    max_x = width - (len(text)*font_size)
+    max_y = height - font_size
+    x, y = random.randint(0, max_x), random.randint(0, max_y)
+    angle = random.uniform(-10, 10)
+    drawer.text((x, y), text, (255, 255, 255), font=font)
+    rotated_text = text_base.rotate(angle)
+    result = Image.alpha_composite(image.convert('RGBA'), rotated_text)
+    return result
 
